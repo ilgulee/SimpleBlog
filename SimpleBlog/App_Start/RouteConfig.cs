43facb20865bd0d86861a02124684c8bca043411
ilgulee <iglee2000ca@gmail.com>
2017-09-16 22:20:58 -0400

    Resolved Conflicts

--- SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SimpleBlog.Controllers;
+
 
 namespace SimpleBlog
 {
@@ -11,11 +13,13 @@ namespace SimpleBlog
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new[] {typeof(PostsController).Namespace};
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("Login", "login", new {controller = "Auth", action = "Login"});
+            routes.MapRoute("Login", "login", new {controller = "Auth", action = "Login"},namespaces);
 
-            routes.MapRoute("Home", "", new {controller = "Posts", action = "Index"});
+            routes.MapRoute("Home", "", new {controller = "Posts", action = "Index"},namespaces);
         }
     }
 }
