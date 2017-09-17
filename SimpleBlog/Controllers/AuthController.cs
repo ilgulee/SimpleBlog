c1c21676bbe75866c28af80812ba6395ff55c2f1
ilgulee <iglee2000ca@gmail.com>
2017-09-17 11:23:06 -0400

    Transferred Data from Form to Controller using ViewModel

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.ViewModel;
 
 namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
-
+        //get
         public ActionResult Login()
         {
             return View();
+        }
+
+        //post
+   
+    [HttpPost]
+    public ActionResult Login(AuthLogin form)
+        {
+            return Content("HI "+form.Username);
         }
     }
 }
