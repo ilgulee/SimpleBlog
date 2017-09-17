ff28be516ebc4788cf0c6112ee6179b2465701f5
ilgulee <iglee2000ca@gmail.com>
2017-09-16 21:45:15 -0400

    Added AuthController and Login Action

--- SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace SimpleBlog
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Login", "login", new {controller = "Auth", action = "Login"});
+
             routes.MapRoute("Home", "", new {controller = "Posts", action = "Index"});
         }
     }
